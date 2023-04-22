015d393465447335aa8921c4a0a60dbc70b70714
Zoltan Kiss <zoltan.kiss.jr@gmail.com>
2023-04-22 23:29:25 +0100

    twoSum

---
+++ leetcode/tests/test_two_sum.py
@@ -0,0 +1,22 @@
+from collections import Counter
+from unittest import TestCase
+
+from leetcode import two_sum
+from leetcode.two_sum import Solution
+
+
+class TestSolution(TestCase):
+    def test_example_1(self):
+        s = Solution()
+        result = s.twoSum([2, 7, 11, 15], 9)
+        self.assertCountEqual(result, [0, 1])
+
+    def test_example_2(self):
+        s = Solution()
+        result = s.twoSum([3, 2, 4], 6)
+        self.assertCountEqual(result, [1, 2])
+
+    def test_example_3(self):
+        s = Solution()
+        result = s.twoSum([3, 3], 6)
+        self.assertCountEqual(result, [0, 1])
---
+++ leetcode/two_sum.py
@@ -0,0 +1,7 @@
+from typing import List
+
+
+# https://leetcode.com/problems/two-sum/
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        return []
