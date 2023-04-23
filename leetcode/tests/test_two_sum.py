a9b12eb8387957207af33759e943c61e92f9d541
Zoltan Kiss <zoltan.kiss.jr@gmail.com>
2023-04-23 21:43:03 +0100

    twoSum

--- leetcode/tests/test_two_sum.py
+++ leetcode/tests/test_two_sum.py
@@ -4,17 +4,24 @@ from leetcode.two_sum import Solution
 
 
 class TestSolution(TestCase):
+    solution = Solution()
+
     def test_example_1(self):
-        s = Solution()
-        result = s.twoSum([2, 7, 11, 15], 9)
+        result = self.solution.twoSum([2, 7, 11, 15], 9)
         self.assertCountEqual(result, [0, 1])
 
     def test_example_2(self):
-        s = Solution()
-        result = s.twoSum([3, 2, 4], 6)
+        result = self.solution.twoSum([3, 2, 4], 6)
         self.assertCountEqual(result, [1, 2])
 
     def test_example_3(self):
-        s = Solution()
-        result = s.twoSum([3, 3], 6)
+        result = self.solution.twoSum([3, 3], 6)
         self.assertCountEqual(result, [0, 1])
+
+    def test_minimal(self):
+        result = self.solution.twoSum([2, 3], 5)
+        self.assertCountEqual(result, [0, 1])
+
+    def test_negatives(self):
+        result = self.solution.twoSum([-3, -1, 0, -4], -3)
+        self.assertCountEqual(result, [0, 2])
--- leetcode/two_sum.py
+++ leetcode/two_sum.py
@@ -4,4 +4,13 @@ from typing import List
 # https://leetcode.com/problems/two-sum/
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        return []
+        dict = {}
+
+        for i in range(len(nums)):
+            needs = target - nums[i]
+            if needs in dict:
+                return [i, dict[needs]]
+
+            dict[nums[i]] = i
+
+        raise Exception("No solution")
