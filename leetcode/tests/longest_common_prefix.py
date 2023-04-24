703f77cec1420087ab3cd391687b1781e6583cdb
Zoltan Kiss <zoltan.kiss.jr@gmail.com>
2023-04-24 21:58:47 +0100

    longestCommonPrefix

---
+++ leetcode/tests/longest_common_prefix.py
@@ -0,0 +1,6 @@
+from typing import List
+
+# https://leetcode.com/problems/longest-common-prefix/
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        raise Exception("not implemented")
---
+++ leetcode/tests/test_longest_common_prefix.py
@@ -0,0 +1,30 @@
+from typing import List
+from unittest import TestCase
+
+from leetcode.tests.longest_common_prefix import Solution
+
+
+class TestSolution(TestCase):
+    def test_example1(self):
+        result = self.longestCommonPrefix(["flower", "flow", "flight"])
+        self.assertEqual(result, "fl")
+
+    def test_example2(self):
+        result = self.longestCommonPrefix(["dog", "racecar", "car"])
+        self.assertEqual(result, "")
+
+    def test_single(self):
+        result = self.longestCommonPrefix(["asdasd"])
+        self.assertEqual(result, "asdasd")
+
+    def test_empty(self):
+        result = self.longestCommonPrefix([""])
+        self.assertEqual(result, "")
+
+    def test_all_same(self):
+        result = self.longestCommonPrefix(["asd", "asd", "asd"])
+        self.assertEqual(result, "asd")
+
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        s = Solution()
+        return s.longestCommonPrefix(strs)
