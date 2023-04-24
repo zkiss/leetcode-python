771950d471c0cce9507ff40f6d42da784ac8da25
Zoltan Kiss <zoltan.kiss.jr@gmail.com>
2023-04-24 22:53:16 +0100

    valid parentheses

--- leetcode/valid_parentheses.py
+++ leetcode/valid_parentheses.py
@@ -1,4 +1,24 @@
 # https://leetcode.com/problems/valid-parentheses/
 class Solution:
     def isValid(self, s: str) -> bool:
-        raise Exception("not implemented")
+        if len(s) % 2 != 0:
+            return False
+
+        pairs = {
+            '(': ')',
+            '{': '}',
+            '[': ']'
+        }
+        stack = []
+
+        for p in s:
+            if p in pairs:  # open
+                stack.append(pairs[p])
+            else:  # close
+                if len(stack) == 0:
+                    return False
+
+                if p != stack.pop():
+                    return False
+
+        return len(stack) == 0
