67e467271e9bcf89c22c408220b25cd9ac5862a1
Zoltan Kiss <zoltan.kiss.jr@gmail.com>
2024-05-01 20:22:09 +0100

    merge sorted arrays

--- leetcode/merge_sorted_arrays.py
+++ leetcode/merge_sorted_arrays.py
@@ -1,6 +1,24 @@
 from typing import List
 
+
 # https://leetcode.com/problems/merge-sorted-array
 class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
-        raise NotImplementedError
+        i = m - 1
+        j = n - 1
+        t = len(nums1) - 1
+
+        while j >= 0:
+            if i < 0:
+                nums1[t] = nums2[j]
+                j -= 1
+            else:
+                a = nums1[i]
+                b = nums2[j]
+                if a > b:
+                    nums1[t] = a
+                    i -= 1
+                else:
+                    nums1[t] = b
+                    j -= 1
+            t -= 1
--- leetcode/tests/test_merge_sorted_arrays.py
+++ leetcode/tests/test_merge_sorted_arrays.py
@@ -27,6 +27,34 @@ class TestSolution(TestCase):
 
         self.assertEqual(num1, [1])
 
+    def test_empties(self):
+        num1 = []
+
+        self.merge(num1, 0, [], 0)
+
+        self.assertEqual(num1, [])
+
+    def test_sames(self):
+        num1 = [-1, -1, 0, 0, 0]
+
+        self.merge(num1, 2, [-1, -1, -1], 3)
+
+        self.assertEqual(num1, [-1, -1, -1, -1, -1])
+
+    def test_reversed(self):
+        num1 = [100, 100, 101, 102, 0, 0, 0]
+
+        self.merge(num1, 4, [1, 2, 3], 3)
+
+        self.assertEqual(num1, [1, 2, 3, 100, 100, 101, 102])
+
+    def test_inorder(self):
+        num1 = [1, 2, 3, 0, 0, 0]
+
+        self.merge(num1, 3, [4, 5, 6], 3)
+
+        self.assertEqual(num1, [1, 2, 3, 4, 5, 6])
+
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int):
         s = Solution()
         s.merge(nums1, m, nums2, n)
