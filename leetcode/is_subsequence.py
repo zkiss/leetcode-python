e173660b33707d621f18d4bc51a8e31a7d918f1e
Zoltan Kiss <zoltan.kiss.jr@gmail.com>
2024-05-04 16:43:39 +0100

    isSubsequence

--- leetcode/is_subsequence.py
+++ leetcode/is_subsequence.py
@@ -3,15 +3,9 @@ class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
         i = 0
         j = 0
-        while i < len(s):
-            a = s[i]
-            while j < len(t) and a != t[j]:
-                j += 1
-
-            if j >= len(t):
-                return False
-
+        while i < len(s) and j < len(t):
+            if s[i] == t[j]:
+                i += 1
             j += 1
-            i += 1
 
         return i == len(s)
