6eeed3158b7f74bd7a6d67ed1af0ea9fd6c0728e
Zoltan Kiss <zoltan.kiss.jr@gmail.com>
2024-07-14 17:29:56 +0100

    https://leetcode.com/problems/determine-if-two-strings-are-close

--- leetcode/determine_if_two_strings_are_close.py
+++ leetcode/determine_if_two_strings_are_close.py
@@ -1,20 +1,11 @@
 # https://leetcode.com/problems/determine-if-two-strings-are-close
 class Solution:
     def closeStrings(self, word1: str, word2: str) -> bool:
-        def counts(iterable) -> dict:
-            cnt = {}
-            for c in iterable:
-                curr = cnt[c] if c in cnt else 0
-                cnt[c] = curr + 1
-            return cnt
-
-        c1 = counts(word1)
-        c2 = counts(word2)
-
-        if c1.keys() != c2.keys():
-            return False
-
-        if counts(c1.values()) != counts(c2.values()):
-            return False
-
-        return True
+        c1 = []
+        c2 = []
+        for c in set(word1):
+            c1.append(word1.count(c))
+            c2.append(word2.count(c))
+        c1.sort()
+        c2.sort()
+        return c1 == c2
